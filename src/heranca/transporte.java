393015d0b5d3ef3328cc052989ae81de113ea20c
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-09-05 20:32:01 -0300

    heranca

---
+++ src/heranca/transporte.java
@@ -0,0 +1,17 @@
+package heranca;
+
+public class transporte {
+
+    private int capacidade;
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    
+
+}
